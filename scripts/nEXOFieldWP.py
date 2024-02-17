fb9d844111c25f3b80cf647f084c7bb008c51d8f
YMtheory <932899789@qq.com>
2024-02-17 14:41:25 -0800

    Implement TAxis objects and TH1 objects for interpolation like in nEXO offline.

--- scripts/nEXOFieldWP.py
+++ scripts/nEXOFieldWP.py
@@ -1,31 +1,27 @@
 import numpy as np
 import uproot as up
-#import ROOT
+import ROOT
 
 class nEXOFieldWP:
     def __init__(self):
-        self.filename = '/Users/yumiao/Documents/Works/0nbb/nEXO/offline-samples/singleStripWP6mm_COMSOL.root'
+        self.filename = '/Users/yumiao/Documents/Works/0nbb/nEXO/offline-samples/weighting_potentials/singleStripWP6mm.root'
         
         self.xaxis = []
         self.yaxis = []
+        self.t_xaxis = None
+        self.t_yaxis = None
         
         self.x = None
         self.y = None
+
+        self.hist = None
         
         
     def GetXBin(self, dx):
-        findId = np.searchsorted(self.xaxis, dx, side='right') 
-        if findId == 1:
-            return findId
-        else:
-            return findId - 1
+        return self.t_xaxis.FindBin(dx)
     
     def GetYBin(self, dy):
-        findId = np.searchsorted(self.yaxis, dy, side='right') 
-        if findId == 1:
-            return findId
-        else:
-            return findId - 1
+        return self.t_yaxis.FindBin(dy)
     
     def GetHist(self, xId, yId):
         name = f'wp_x{xId}_y{yId}'
@@ -36,15 +32,25 @@ class nEXOFieldWP:
         cents = (edges[1:] + edges[:-1]) / 2.
         self.x, self.y = cents, conts
 
+        self.hist = ROOT.TH1F(name, "WP histogram", len(edges)-1, edges)
+        for i, cont in enumerate(conts):
+            self.hist.SetBinContent(i+1, cont)
+            
+
+
     
     def interpolate(self, z):
-        return np.interp(z, self.x, self.y)
+        #return np.interp(z, self.x, self.y)
+        return self.hist.Interpolate(z)
     
     
     def _load_axes(self):
         f = up.open(self.filename)
         self.xaxis = f['xaxis'].edges()
         self.yaxis = f['yaxis'].edges()
+
+        self.t_xaxis = ROOT.TAxis(len(self.xaxis)-1, self.xaxis[0], self.xaxis[-1])
+        self.t_yaxis = ROOT.TAxis(len(self.yaxis)-1, self.yaxis[0], self.yaxis[-1])
         
 
         

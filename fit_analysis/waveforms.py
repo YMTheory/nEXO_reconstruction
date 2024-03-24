b936cd4dd76eacbf0c417f7a934a9cd7fde1e860
YMTheory <932899789@qq.com>
2024-03-24 16:37:48 -0700

    Draw fitting waveforms for single event.

---
+++ fit_analysis/waveforms.py
@@ -0,0 +1,32 @@
+from draw import drawer
+from loadFits import loader
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+ld = loader(inductive=False, noise=True, sscut=1.0, threshold=40, day=20)
+d = drawer()
+
+#filename = '/fs/ddn/sdf/group/nexo/users/miaoyu/Reconstruction/Data/Sim/Baseline2019_bb0n_center_seed29_newmap_comsol.nEXOevents.root'
+#ld.set_one_file(filename)
+
+n = 1
+for i in range(n):
+
+    filename, df_fit_oneevt = ld.get_one_event()
+    
+    print(f'===== Loading event {i} =====')
+    print(df_fit_oneevt)
+    
+    rootfile = ld.corresponding_rootfile()
+
+    d.config_fitter(rootfile, int(df_fit_oneevt['evtid']),ld.inductive, ld.noise, ld.fine_no, ld.sscut, ld.threshold)
+    d.config_fitVals(df_fit_oneevt['fitt'], df_fit_oneevt['fitx'], df_fit_oneevt['fity'], df_fit_oneevt['fitQ'])
+    d.fitting_strip_charge()
+
+    d.draw_channel_waveforms()
+
+
+
+
+
